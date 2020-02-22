30f98288a9932119cfd9a0a92ea5c8587b1a10ba
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2020-02-23 00:20:45 +0900

    set up CRUD actions, model, relation and routes for user and projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_21_040417) do
+ActiveRecord::Schema.define(version: 2020_02_22_060211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
 
   create_table "stickers", force: :cascade do |t|
     t.string "name"
@@ -33,4 +41,5 @@ ActiveRecord::Schema.define(version: 2020_02_21_040417) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "projects", "users"
 end
